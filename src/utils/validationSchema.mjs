97da33e7c2f287d9dd559e5f42f5011290e4d013
Clarence Coronel <clarence.coronel.r@gmail.com>
2024-06-02 13:53:44 +0800

    feat: add new validation schema for login endpoint

--- src/utils/validationSchema.mjs
+++ src/utils/validationSchema.mjs
@@ -43,3 +43,36 @@ export const createUserValidationSchema = {
     },
   },
 };
+
+export const loginUserValidationSchema = {
+  username: {
+    isLength: {
+      options: {
+        min: 5,
+        max: 32,
+      },
+      errorMessage: "Username must be at least 5-32 characters",
+    },
+    notEmpty: {
+      errorMessage: "Username cannot be empty",
+    },
+    isString: {
+      errorMessage: "Username must be a string",
+    },
+  },
+  password: {
+    isLength: {
+      options: {
+        min: 5,
+        max: 32,
+      },
+      errorMessage: "Password must be at least 5-32 characters",
+    },
+    notEmpty: {
+      errorMessage: "Password cannot be empty",
+    },
+    isString: {
+      errorMessage: "Password name must be a string",
+    },
+  },
+};
