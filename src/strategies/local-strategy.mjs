6c66215fffbe3fb0319c0413b1f3c7ca7fda9025
Clarence Coronel <clarence.coronel.r@gmail.com>
2024-06-02 13:52:06 +0800

    feat: add user authentication

---
+++ src/strategies/local-strategy.mjs
@@ -0,0 +1,43 @@
+import passport from "passport";
+import { Strategy } from "passport-local";
+import { User } from "./../mongoose/schemas/UserSchema.mjs";
+import { comparePassword } from "../utils/helpers.mjs";
+
+passport.serializeUser((user, done) => {
+  //   console.log(`Inside Serialize User`);
+  //   console.log(user);
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  //   console.log(`Inside Deserializer`);
+  //   console.log(`Deserializing User ID: ${id}`);
+  try {
+    const findUser = await User.findById(id);
+    if (!findUser) throw new Error("User Not Found");
+
+    const userWithoutPassword = findUser.toObject();
+    delete userWithoutPassword.password;
+
+    done(null, userWithoutPassword);
+  } catch (error) {
+    done(error, null);
+  }
+});
+
+export default passport.use(
+  new Strategy(async (username, password, done) => {
+    try {
+      const findUser = await User.findOne({ username });
+
+      if (!findUser) throw new Error("User Not Found");
+
+      if (!comparePassword(password, findUser.password))
+        throw new Error("Bad Credentials");
+
+      done(null, findUser);
+    } catch (error) {
+      done(error, null);
+    }
+  })
+);
