f3604d5a0f91b9c87473a8680ca564d4260b8c07
Clarence Coronel <clarence.coronel.r@gmail.com>
2024-06-02 13:52:38 +0800

    feat: create endpoint for login, check status, logout

--- src/routes/user.mjs
+++ src/routes/user.mjs
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { Router, response } from "express";
 import {
   query,
   validationResult,
@@ -6,10 +6,39 @@ import {
   matchedData,
 } from "express-validator";
 import { User } from "../mongoose/schemas/UserSchema.mjs";
-import { createUserValidationSchema } from "../utils/validationSchema.mjs";
+import {
+  createUserValidationSchema,
+  loginUserValidationSchema,
+} from "../utils/validationSchema.mjs";
+import "./../strategies/local-strategy.mjs";
 import { hashPassword } from "../utils/helpers.mjs";
+import passport from "passport";
 
 const router = Router();
+
+router.post(
+  "/api/user/auth",
+  checkSchema(loginUserValidationSchema),
+  async (request, response, next) => {
+    const result = validationResult(request);
+
+    if (!result.isEmpty())
+      return response.status(400).send({ errors: result.array() });
+
+    next();
+  },
+  passport.authenticate("local"),
+  async (request, response) => {
+    return response.sendStatus(200);
+  }
+);
+
+router.get("/api/user/auth/status", (request, response) => {
+  console.log(`Inside /auth/status endpoint`);
+  console.log(request.user);
+  console.log(request.session);
+  return request.user ? response.send(request.user) : response.sendStatus(401);
+});
 
 router.post(
   "/api/user/register",
@@ -34,5 +63,15 @@ router.post(
     }
   }
 );
+
+router.post("/api/user/auth/logout", (request, response) => {
+  if (!request.user) return response.sendStatus(401);
+
+  request.logout((err) => {
+    if (err) return response.sendStatus(400);
+
+    return response.sendStatus(200);
+  });
+});
 
 export default router;
